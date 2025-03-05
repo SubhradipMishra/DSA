f0b725fcac1fe2a0ac17e3a537f4d487e7a8d865
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-06 01:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (97.99%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,33 +1,23 @@
+
 class Solution {
 public:
+
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return 0;
 
-        int count = 0;  
-        int jump = 0;  
+      for(int i = 1; i < nums.size(); i++)
+      {
+        nums[i] = max(nums[i] + i, nums[i-1]);
+      }
 
-        for (int i = 0; i < n; ) {
-            if (i + nums[i] >= n - 1) {
-                count++;
-                break;
-            }
+      int ind = 0;
+      int ans = 0;
 
-            int x = i + nums[i]; 
-            int maxE = INT_MIN;
-            int nextPos = i;
+      while(ind < nums.size() - 1)
+      {
+        ans++;
+        ind = nums[ind];
+      }
 
-            for (int j = i + 1; j <= x && j < n; j++) {
-                if (j + nums[j] > maxE) {
-                    maxE = j + nums[j];
-                    nextPos = j;
-                }
-            }
-
-            i = nextPos;
-            count++;  
-        }
-
-        return count; 
+      return ans;
     }
 };
