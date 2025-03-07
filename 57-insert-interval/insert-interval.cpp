942887286b7e96fe17af1b24209571ba62957d88
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-08 02:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (21.96%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& newInterval) {
-        int n  = arr.size(); 
-        vector<vector<int>> ans ; 
-        int i =  0 ;
-        while(i <   n && arr[i][1] < newInterval[0]){
+        int n = arr.size() ; 
+        int i =  0 ; 
+        vector<vector<int>> ans  ; 
+        while(i < n && arr[i][1]  < newInterval[0]){
             ans.push_back(arr[i]) ;
-            i+=1;
+            i++ ;
         }
-         while(i <   n && arr[i][0] <= newInterval[1]){
-            newInterval[0]= min(newInterval[0],arr[i] [0]) ;
-            newInterval[1]= max(newInterval[1],arr[i] [1]) ;
-            i+=1;
-         }
-         ans.push_back(newInterval) ; 
-         while(i < n ) {
-            ans.push_back(arr[i]) ; 
-            i+=1;
-         }
+        while(i <  n  && arr[i][0] <= newInterval[1]){
+            newInterval[0] = min(arr[i][0] , newInterval[0]) ;
+            newInterval[1] = max(arr[i][1] , newInterval[1]) ;
+            i++ ; 
+            
 
-         return ans ; 
+        }
+        ans.push_back(newInterval) ; 
+
+        while(i <  n) {
+            ans.push_back(arr[i]); 
+            i++ ; 
+        }
+
+        return ans  ;
     }
+
+
 };
