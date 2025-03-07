fdaf60410326bcdfd4614f4c9dd46b3af91b8820
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-08 01:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (46.34%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& arr, vector<int>& newInterval) {
-        int n = arr.size();
-        vector<vector<int>> ans;
-        int i = 0;
-
-        // Add all intervals that come before newInterval
-        while (i < n && arr[i][1] < newInterval[0]) {
-            ans.push_back(arr[i]);
-            i++;
+        int n  = arr.size(); 
+        vector<vector<int>> ans ; 
+        int i =  0 ;
+        while(i <   n && arr[i][1] < newInterval[0]){
+            ans.push_back(arr[i]) ;
+            i+=1;
         }
+         while(i <   n && arr[i][0] <= newInterval[1]){
+            newInterval[0]= min(newInterval[0],arr[i] [0]) ;
+            newInterval[1]= max(newInterval[1],arr[i] [1]) ;
+            i+=1;
+         }
+         ans.push_back(newInterval) ; 
+         while(i < n ) {
+            ans.push_back(arr[i]) ; 
+            i+=1;
+         }
 
-        while (i < n && arr[i][0] <= newInterval[1]) {  // Changed '<' to '<='
-            newInterval[0] = min(newInterval[0], arr[i][0]);
-            newInterval[1] = max(newInterval[1], arr[i][1]);
-            i++;
-        }
-        ans.push_back(newInterval);
-
-        // Add remaining intervals
-        while (i < n) {
-            ans.push_back(arr[i]);
-            i++;
-        }
-
-        return ans;
+         return ans ; 
     }
 };
