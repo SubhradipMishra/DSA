c2fc50c0feb15c04c1856cac5c804f133f7dabde
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-08 19:03:56 +0530

    Time: 11 ms (16.63%) | Memory: 26.1 MB (6.01%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-       int n = arr.size(); // size of the array
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans; 
+        vector<vector<int>> m; 
+        
+        // Sort intervals to ensure proper merging
+        sort(intervals.begin(), intervals.end());
 
-    //sort the given intervals:
-    sort(arr.begin(), arr.end());
+        int endTime = intervals[0][1]; 
+        m.push_back(intervals[0]);
 
-    vector<vector<int>> ans;
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] > endTime) {
+                // Merge all stored intervals in `m` before starting a new merge
+                vector<int> newInterval(2);
+                newInterval[0] = INT_MAX;
+                newInterval[1] = INT_MIN;
 
-    for (int i = 0; i < n; i++) {
-        // if the current interval does not
-        // lie in the last interval:
-        if (ans.empty() || arr[i][0] > ans.back()[1]) {
-            ans.push_back(arr[i]);
+                for (int j = 0; j < m.size(); j++) {
+                    newInterval[0] = min(m[j][0], newInterval[0]);
+                    newInterval[1] = max(m[j][1], newInterval[1]);
+                }
+                ans.push_back(newInterval);
+                
+                // Clear `m` and start fresh for the next set of intervals
+                m.clear();
+                m.push_back(intervals[i]);
+            } else {
+                m.push_back(intervals[i]);
+            }
+            endTime = max(endTime, intervals[i][1]);
         }
-        // if the current interval
-        // lies in the last interval:
-        else {
-            ans.back()[1] = max(ans.back()[1], arr[i][1]);
+
+        // Merge the last group of intervals in `m`
+        vector<int> newInterval(2);
+        newInterval[0] = INT_MAX;
+        newInterval[1] = INT_MIN;
+
+        for (int j = 0; j < m.size(); j++) {
+            newInterval[0] = min(m[j][0], newInterval[0]);
+            newInterval[1] = max(m[j][1], newInterval[1]);
         }
-    }
-    return ans;
+        ans.push_back(newInterval);
+
+        return ans;
     }
 };
