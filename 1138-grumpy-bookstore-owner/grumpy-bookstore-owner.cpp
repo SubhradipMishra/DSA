2c4040f1eb4a98d89132ef7fb1c41206bbb8b049
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-17 10:05:06 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (73.00%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& arr, vector<int>& grumpy, int k) {
-        int n = arr.size() ; 
-        int prevLoss = 0  ; 
+      int n  =  arr.size() ; 
+      int prevLoss = 0  ; 
+      for(int i =   0 ; i  <  k ; i++) {
+       if(grumpy[i] ==  1 )  prevLoss+= arr[i]  ;
+      }
+         int maxLoss =  prevLoss ; 
+         int maxIdx  = 0 ; 
+         int i  = 1 ; 
+         int j =  k ;
+         while(j  < n){
+           int currLoss = prevLoss ;
+           if(grumpy[j] == 1) currLoss +=  arr[j] ; 
+           if(grumpy[i-1]==  1)  currLoss-=arr[i-1]  ;
+           if(maxLoss <  currLoss )  {
+            maxLoss = currLoss ; 
+            maxIdx = i ; 
+           }
+           prevLoss = currLoss ; 
+           i++;
+           j++ ; 
 
-        for(int  i = 0 ; i < k  ; i++ ) {
-            if(grumpy[i] == 1 ) prevLoss+= arr[i] ; 
+         }
 
-        }
+         for(int i  = maxIdx  ; i   <  maxIdx +   k  ; i++){
+             grumpy[i] = 0 ; 
+         }
 
-        int maxLoss = prevLoss ; 
-        int maxIdx  =  0 ; 
-        int  i = 1; 
-        int  j = k ; 
-        while( j < n ) {
-             int curLoss  = prevLoss ; 
-             if(grumpy[j] == 1 )  curLoss  += arr[j] ; 
-              if(grumpy[i-1] == 1 )  curLoss  -= arr[i-1] ; 
+         int maxS =   0 ;
+         for(int  i =  0 ;  i  <  n ; i++ ) {
+            if( grumpy[i] == 0 )  maxS+=arr[i] ; 
+         }
 
 
-               if (maxLoss < curLoss) {
-                maxLoss = curLoss;
-                maxIdx = i;
-            }
-            prevLoss = curLoss;
-            i++;
-            j++;
-        }
- for (int i = maxIdx; i < maxIdx + k; i++) {
-            grumpy[i] = 0;
-        }
+         return maxS ; 
 
- int maxS = 0;
-        for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) maxS += arr[i];
-        }
 
-return maxS ; 
     }
 };
