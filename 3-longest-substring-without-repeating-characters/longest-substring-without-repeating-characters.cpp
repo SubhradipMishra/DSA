275d145c85173f0432aea7ea83c73bdf3b28886a
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-20 18:51:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (81.86%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -7,8 +7,9 @@ public:
       int n = s.size();
       int len = 0;
       while (right < n) {
-        if (mpp[s[right]] != -1)
+        if (mpp[s[right]] != -1){
           left = max(mpp[s[right]] + 1, left);
+        }
 
         mpp[s[right]] = right;
 
