caf0770a4bd691b00ca3bf1818706d81d2cdc206
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-03-24 11:54:10 +0530

    Time: 27 ms (30.22%) | Memory: 11.3 MB (20.56%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> lastSeen(3, -1);
-        int count = 0;
+        string str = "abc"; 
+        unordered_map<char, int> mp; 
+        
+        for (int i = 0; i < 3; i++) {
+            mp[str[i]] = -1;
+        } 
 
+        int count = 0;
         for (int i = 0; i < s.size(); i++) {
-            lastSeen[s[i] - 'a'] = i;
-            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
-                count += 1 + min({lastSeen[0], lastSeen[1], lastSeen[2]});
+            mp[s[i]] = i;  
+
+            if (mp['a'] != -1 && mp['b'] != -1 && mp['c'] != -1) {
+                count += 1 + min({mp['a'], mp['b'], mp['c']});
             }
         }
+
         return count;
     }
 };
-
-
-        
