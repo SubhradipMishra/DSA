6a556c4ba229e3b3d72f6320b1b0b9af28399764
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-01 18:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (84.84%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,60 +1,28 @@
 class Solution {
 public:
-    // int f(int  i , int j ,vector<vector<int>>& matrix , vector<vector<int>>& dp ){
-    //  // base case :- 
-    //    if(j < 0  || j  >= matrix[0].size()) return 1e8 ; 
-    //    if (i == 0) return matrix[0][j];
-
-    //    // 
-
-    //    if(dp[i][j]!=-1) return dp[i][j] ;
-
-    //    int u = matrix[i][j] + f(i-1,j ,matrix ,dp ); //  go up
-    //    int ld = matrix[i][j] + f(i-1,j-1 ,matrix , dp ) ; // go left digonal
-    //    int rd = matrix[i][j] + f(i-1,j+1 ,matrix , dp ) ; // go right digonal 
-
-    //    return dp[i][j] = min(u,min(ld,rd)) ;
-    // }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int   n = matrix.size() ; 
-        int   m = matrix[0].size() ; 
-        vector<vector<int>>dp(n , vector<int>(m,0)) ; 
-       
-       
-       
-       // base case :- 
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> prev(matrix[0]); // Initialize with the first row
 
-    for(int  j =  0 ; j< m ; j++){
-        dp[0][j] =  matrix[0][j] ;
-    }
+        vector<int> cur(m, 0);
 
-    for(int i  = 1  ; i  < n  ; i++){
-         for(int j = 0 ; j  <  m ; j++) {
-           int u = matrix[i][j] + dp[i-1][j];
-             int ld = matrix[i][j]  ;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int u = matrix[i][j] + prev[j];
+                int ld = matrix[i][j];
+                if (j - 1 >= 0) ld += prev[j - 1];
+                else ld += 1e8;  // Large value to avoid selecting invalid paths
+                
+                int rd = matrix[i][j];
+                if (j + 1 < m) rd += prev[j + 1];
+                else rd += 1e8;
 
-        if(j-1>=0) ld+=dp[i-1][j-1] ;
-        else ld+=1e8 ; 
-        int rd = matrix[i][j] ;
-        if(j+1<m) rd+=dp[i-1][j+1]; 
-        else rd+=1e8 ; 
-        dp[i][j] =min(u,min(ld,rd)); 
-         }
-    }
-       
-       
-       
-       
-       
-        int mini = 1e8 ; 
-
-        for(int  j =  0 ; j <  m  ;j++){
-            mini = min(mini , dp[n-1][j]);
+                cur[j] = min(u, min(ld, rd));
+            }
+            prev = cur;
         }
 
-
-        return mini ;
-
-
+        return *min_element(prev.begin(), prev.end());
     }
 };
