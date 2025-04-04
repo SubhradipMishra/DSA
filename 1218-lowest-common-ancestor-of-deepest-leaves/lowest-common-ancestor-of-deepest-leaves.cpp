38f47f0598df24cff30208491ffa9e7c1d61672e
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-04 09:46:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (39.60%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,19 +1,26 @@
 
 class Solution {
 public:
-     int depth(TreeNode* root){
-        if(root==NULL)return 0; 
-        int l=depth(root->left); 
-        int r=depth(root->right); 
-         return max(l,r)+1;
-     }
+int depth(TreeNode* root){
+    if(root == NULL )  return  0 ;
+    int r = depth(root->right) ;
+    int l = depth(root->left)  ;
+
+    return max(l,r)+1 ; 
+
+}
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        int retl = depth(root->left) ,retr  = depth(root->right) ;
-        if(retl  == retr)  return root ; 
 
-      else if(retl > retr) root = root->left ; 
-       else if(retl < retr) root = root->right ; 
-       return lcaDeepestLeaves(root)  ;
-    }
 
+     int r = depth(root->right) ;
+     int l = depth(root->left)  ;
+
+        if(l == r)  return root ; 
+       else if(l  > r ) root = root->left  ;
+       else if(r > l ) root = root->right ; 
+        
+
+        return lcaDeepestLeaves(root) ;  
+        
+            }
 };
