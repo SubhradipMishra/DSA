40621212552fd11dbe9dc226226fd8140d6059c6
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-13 16:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,7 +6,14 @@ public:
     return dp[i] = f(i-1,dp) + f(i-2,dp);
    }
     int fib(int n) {
-       vector<int>dp(n+1 , -1) ; 
-       return f(n , dp)  ; 
+       vector<int>dp(n+1) ; 
+     if(n ==0 || n ==1)  return n ;
+     dp[0] = 0 ; 
+     dp[1] = 1 ; 
+     for(int i  = 2 ; i <= n ;i++){
+        dp[i] = dp[i-2] + dp[i-1] ;
+     }
+
+     return dp[n]; 
     }
 };
