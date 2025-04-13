f809722c207c47934741b162e316490862730d2a
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-13 18:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.25%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
+    int robLinear(vector<int>& nums, int start, int end, vector<int>& dp) {
+        if (start > end) return 0;
+        if (dp[start] != -1) return dp[start];
 
-long long int solve(vector<int>& arr){
-    int n = arr.size();
-    long long int prev = arr[0];
-    long long int prev2 =0;
-    
-    for(int i=1; i<n; i++){
-        long long int pick = arr[i];
-        if(i>1)
-            pick += prev2;
-        int long long nonPick = 0 + prev;
-        
-        long long int cur_i = max(pick, nonPick);
-        prev2 = prev;
-        prev= cur_i;
-        
+        int robCurrent = nums[start] + robLinear(nums, start + 2, end, dp);
+        int skipCurrent = robLinear(nums, start + 1, end, dp);
+
+        return dp[start] = max(robCurrent, skipCurrent);
     }
-    return prev;
-}
-    int rob(vector<int>& arr) {
-        int n = arr.size() ;
-        vector<int> arr1;
-    vector<int> arr2;
-    
-    if(n==1)
-       return arr[0];
-    
-    for(int i=0; i<n; i++){
-        
-        if(i!=0) arr1.push_back(arr[i]);
-        if(i!=n-1) arr2.push_back(arr[i]);
-    }
-    
-    long long int ans1 = solve(arr1);
-    long long int ans2 = solve(arr2);
-    
-    return max(ans1,ans2);
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        // Exclude first house
+        vector<int> dp1(n, -1);
+        int rob1 = robLinear(nums, 1, n - 1, dp1);
+
+        // Exclude last house
+        vector<int> dp2(n, -1);
+        int rob2 = robLinear(nums, 0, n - 2, dp2);
+
+        return max(rob1, rob2);
     }
 };
