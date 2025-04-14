930e5f73399dfd148677bd210050dc564377be98
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-15 00:51:19 +0530

    Time: 4 ms (15.95%) | Memory: 16.2 MB (56.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& grid , vector<vector<int>>& dp) {
-        if (i == 0 && j == 0) return grid[0][0]; 
-        if (i < 0 || j < 0) return INT_MAX; 
-        if(dp[i][j] !=-1) return dp[i][j] ;
+//    int f(int m , int n ,vector<vector<int>>& grid , vector<vector<int>>& dp ){
+//     if(m == 0 && n == 0  ) return grid[0][0] ; 
+//     if(m < 0 || n < 0  ) return INT_MAX ;  
+//     if(dp[m][n]!=-1) return dp[m][n] ;
+//     int up = f(m-1 , n  , grid , dp) ;
+//     int left = f(m , n-1,grid,dp) ; 
+//     return dp[m][n]  = grid[m][n] +  min(up,left) ; 
+//    }
+    int minPathSum(vector<vector<int>>& grid) {
+             int n =  grid.size() ; 
+        int m = grid[0].size()  ;
+     vector<vector<int>> dp(n, vector<int>(m, 0)); 
 
-        int left = f(i - 1, j, grid,dp);
-        int up = f(i, j - 1, grid,dp);
+     for(int i =   0 ; i <   n ; i++)  {
+        for(int j  = 0  ; j <   m ;j++) {
+            if( i == 0 && j == 0 ) dp[i][j] = grid[i][j]; 
+            else{
+                int up = grid[i][j] ; 
+                if(i >  0 )  up += dp[i-1][j]  ;
+                 else up += 1e9;
 
-        return dp[i][j] = grid[i][j] + min(left, up);
-    }
-
-    int minPathSum(vector<vector<int>>& matrix) {
-        int n =  matrix.size() ; 
-        int m = matrix[0].size()  ;
-     vector<vector<int>> dp(n, vector<int>(m, 0)); // Initialize a DP table to store minimum path sums
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (i == 0 && j == 0)
-                dp[i][j] = matrix[i][j]; // If we are at the top-left corner, the minimum path sum is the value at (0, 0)
-            else {
-                // Calculate the minimum path sum considering moving up and moving left
-                int up = matrix[i][j];
-                if (i > 0)
-                    up += dp[i - 1][j]; // Include the minimum path sum from above
-                else
-                    up += 1e9; // A large value if moving up is not possible (out of bounds)
-
-                int left = matrix[i][j];
-                if (j > 0)
-                    left += dp[i][j - 1]; // Include the minimum path sum from the left
-                else
-                    left += 1e9; // A large value if moving left is not possible (out of bounds)
-
-                // Store the minimum path sum in dp[i][j]
+                 int left = grid[i][j]  ;
+                 if(j > 0 ) left += dp[i][j - 1]; 
+                 else left += 1e9;
                 dp[i][j] = min(up, left);
-            }
-        }
-    }
 
-    // The final result is stored in dp[n-1][m-1], which represents the destination
-    return dp[n - 1][m - 1];
+             }
+        }
+     }
+
+
+     return dp[n-1][m-1];
     }
 };
