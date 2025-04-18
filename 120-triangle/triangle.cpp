18a6c589a12e33aa3a46430eb5e71db4e67b97c4
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-18 07:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (83.43%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -7,23 +7,25 @@ public:
     //     int dg = t[i][j] + f(i+1,j+1,t,dp) ;
     //     return dp[i][j] = min(d,dg) ; 
     // }
-    int minimumTotal(vector<vector<int>>& t) {
-        int n =  t.size() ; 
-        vector<vector<int>> dp(n, vector<int>(n));
-         
-         // base case
-                for(int j =  0 ; j<  n ;j++){
-                    dp[n-1][j]= t[n-1][j] ;
-                }
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n = triangle.size();
+    
+    
+    vector<int> front(n), cur(n);
 
-                for(int i  = n-2 ;i>=0 ;i--){
-                    for(int j = i ;j >= 0 ;j--){
-                        int d = t[i][j] + dp[i+1][j] ;
-                        int dg = t[i][j] + dp[i+1][j+1] ;
-                        dp[i][j]  = min(d,dg) ;
-                        
-                    }
-                }
-         return dp[0][0];
+    
+    for(int j = 0; j < n; j++) 
+        front[j] = triangle[n-1][j];
+
+    for(int i = n-2; i >= 0; i--) {
+        for(int j = i; j >= 0; j--) {
+            int d = triangle[i][j] + front[j];
+            int dg = triangle[i][j] + front[j+1];
+            cur[j] = min(d, dg);
+        }
+        front = cur;
     }
+
+    return front[0];
+}
 };
