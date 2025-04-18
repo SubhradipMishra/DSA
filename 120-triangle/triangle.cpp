96ad336c79e69eda49a9cff08e2edf2be1878a43
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-18 07:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (10.40%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-   int f(int i  , int j ,vector<vector<int>>& t , vector<vector<int>>&dp ,int n ){
-   if(i  ==  n-1){
-    return t[n-1][j] ;
-   }
-   if(dp[i][j]!=-1) return dp[i][j] ;
-   int d = t[i][j] + f(i+1 ,j,t ,dp,n) ;
-   int dg = t[i][j] + f(i+1 ,j +1,t,dp,n) ; 
-   return dp[i][j] = min(d,dg) ; 
-   }
+    int f(int i , int j ,vector<vector<int>>& t  , vector<vector<int>>& dp){
+        if(i == t.size()-1) return t[t.size()-1][j] ;
+        if(dp[i][j]!=-1) return dp[i][j] ; 
+        int d = t[i][j] + f(i+1,j,t,dp) ;
+        int dg = t[i][j] + f(i+1,j+1,t,dp) ;
+        return dp[i][j] = min(d,dg) ; 
+    }
     int minimumTotal(vector<vector<int>>& t) {
-        int n = t.size() ; 
-       vector<vector<int>> dp(n, vector<int>(n, -1));
-       return f(0,0,t ,dp,n) ;
+        int n =  t.size() ; 
+         vector<vector<int>> dp(n, vector<int>(n, -1));
 
+         return f(0,0,t,dp) ;
     }
 };
