0f9e3e6a3d886dfdd8409c999e49183d6d5fbecb
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-24 08:17:19 +0530

    Time: 15 ms (75.00%) | Memory: 10.5 MB (78.44%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<uint64_t>> dp(n, vector<uint64_t>(amount + 1, 0));
-
+        // vector<vector<uint64_t>> dp(n, vector<uint64_t>(amount + 1, 0));
+          vector<uint64_t>prev(amount+1 , 0)  ; 
+          vector<uint64_t>curr(amount+1 , 0)  ;
         // Base case for the first coin
         for (int T = 0; T <= amount; ++T) {
-            if (T % coins[0] == 0) dp[0][T] = 1;
+            if (T % coins[0] == 0) prev[T] = 1;
         }
 
         // Main DP transitions
         for (int idx = 1; idx < n; ++idx) {
             for (int T = 0; T <= amount; ++T) {
-                uint64_t notTake = dp[idx - 1][T];
+                uint64_t notTake = prev[T];
                 uint64_t take = 0;
                 if (coins[idx] <= T)
-                    take = dp[idx][T - coins[idx]];
-                dp[idx][T] = take + notTake;
+                    take = curr[T - coins[idx]];
+                curr[T] = take + notTake;
             }
+            prev  =curr  ;
         }
 
-        return (int)dp[n - 1][amount];
+        return (int)prev[amount];
     }
 };
