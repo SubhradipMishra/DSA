890ecc46f9af7e3d48597daf832afc13b6ba20f2
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-24 06:50:00 +0530

    Time: 15 ms (80.59%) | Memory: 41.8 MB (59.44%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
-        unordered_set<int>s ;
-        for(int i   = 0  ;i  < nums.size() ;i++){
-            s.insert(nums[i]);
-        }
-        int total = s.size()  ;
-        int count = 0 ;
-        for(int i =  0 ; i <  nums.size() ; i++){
-              unordered_set<int>curr ; 
-          for(int j = i; j <  nums.size() ; j++){
-            curr.insert(nums[j])  ;
-            if(curr.size() ==  total){
-                count+= (nums.size()-j)  ;
-                break ;
-            }
-          }
-        }
-       return count ; 
+        int n = nums.size();
+        unordered_set<int> s(nums.begin(), nums.end());
+        int k = s.size(); // total unique elements
+        
+        unordered_map<int, int> freq;
+        int count = 0;
+        int left = 0;
 
+        for (int right = 0; right < n; ++right) {
+            freq[nums[right]]++;
+            
+            while (freq.size() == k) {
+                count += (n - right); // all subarrays from left to right...n-1
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+                left++;
+            }
+        }
+        return count;
     }
 };
