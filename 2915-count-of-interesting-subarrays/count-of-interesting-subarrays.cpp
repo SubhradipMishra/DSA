bcfefec5335943f195e55269eff6ecd6c6dc5592
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-04-25 07:23:59 +0530

    Time: 65 ms (34.43%) | Memory: 122.7 MB (59.02%) - LeetSync

---
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+       unordered_map<int, int> freq;
+        freq[0] = 1;
+        long long res = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num % modulo == k)
+                count = (count + 1) % modulo;
+            
+            int needed = (count - k + modulo) % modulo;
+            res += freq[needed];
+
+            freq[count]++;
+        }
+
+        return res;
+    }
+};
