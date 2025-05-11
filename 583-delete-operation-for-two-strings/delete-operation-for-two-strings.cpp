f3725660a922f65af26afce1de1e324a7670bd14
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-05-12 00:47:24 +0530

    Time: 13 ms (43.72%) | Memory: 16.7 MB (71.99%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -9,9 +9,18 @@ public:
     int minDistance(string s, string t) {
         int n  = s.size() ; 
         int m  = t.size() ; 
-        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1)) ; 
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , 0)) ; 
+        for(int  i = 0 ; i <=  n ; i++ ) dp[i][0] = 0 ; 
+        for(int j =  0 ; j <= m ;j++ )dp[0][j] = 0 ; 
 
-        int lcs =  f(n-1 , m-1 ,s,t,dp) ; 
+        for(int i = 1 ; i<= n; i++){
+            for(int j = 1 ; j<=m ;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]  =  1 + dp[i-1][j-1]; 
+                else dp[i][j] = 0 +  max(dp[i-1][j]  ,dp[i][j-1])  ;
+            }
+        }
+        
+        int lcs =  dp[n][m] ; 
         int ans = (n + m) - (2*lcs) ;
         return ans ; 
     }
