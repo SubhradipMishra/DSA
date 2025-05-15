8af93437ace5862dd5cd0aeffb6edb7ff4447d7f
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-05-15 21:51:23 +0530

    Time: 4 ms (81.14%) | Memory: 13.1 MB (61.30%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -18,7 +18,24 @@ public:
     int minDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, s, t, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // base case
+
+        for(int i =  0 ; i <=  n ; i++ ) dp[i][0] = i ; 
+        for(int j =  0  ; j <= m ; j++ ) dp[0][j] = j ; 
+
+        for(int i  = 1 ; i <= n ; i++){
+            for(int j =  1 ;j <= m ; j++){
+                if(s[i-1] ==  t[j-1] ) dp[i][j] =  dp[i-1][j-1] ;
+                else{
+                    int ins = dp[i][j-1];     // Insert
+                    int del = dp[i-1][j];     // Delete
+                    int rep = dp[i-1][j-1];  // Replace
+                    dp[i][j] = 1 + min(ins,min(del,rep));
+                }
+            }
+        }
+
+        return dp[n][m] ;
     }
 };
