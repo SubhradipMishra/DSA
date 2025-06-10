068120a55d6bea230a4d7e9cd17b41ec2c8e3bdb
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-06-10 07:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.02%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    int bs(int lo , int hi ,vector<int>& nums, int k ){
-        while(lo <= hi ){
-            int mid = lo + (hi-lo)/2 ;
-            if(nums[mid] == k ) return mid ; 
-            else if(nums[mid]  < k  ) lo = mid + 1 ;
-            else hi = mid -1 ; 
-        }
-        return -1;
-    }
-    int minIdx(vector<int>& nums){
-        int lo =  0 ;
-        int  n = nums.size() ;
-        int hi = n - 1 ; 
-        while(lo <= hi ){
-            if(nums[lo] <= nums[hi] )  return lo ; 
-            int mid = lo + (hi - lo )/2; 
-            int next = ( mid +1 ) % n ; 
-            int prev = (mid -1 + n ) % n ;
-            if(nums[mid] < nums[prev] && nums[mid] <  nums[next] ) return mid ; 
-            else if(nums[mid] >=  nums[lo]) lo = mid + 1; 
-            else hi  =  mid-1; 
-        }
-
-        return -1;
-        
-    }
     int search(vector<int>& nums, int k) {
-        int min_idx = minIdx(nums);
         int n = nums.size() ; 
-        int ans1 = bs(0,min_idx-1,nums,k)  ;
-        int ans2 = bs(min_idx,n-1,nums,k)  ;
-        if(ans1 == -1) return ans2 ;
-       else if(ans2 == -1) return ans1 ;
-       else return -1;
+        int lo = 0 ;
+        int hi = n-1; 
+         while(lo <= hi ) {
+            int mid = lo + (hi - lo )/2 ;
+            if(nums[mid] == k ) return mid; 
+            else if(nums[lo] <= nums[mid]){
+                if(nums[lo] <= k &&   k <= nums[mid]) hi = mid-1;
+                else lo = mid +1; 
+            }
+            else{
+                if(nums[mid] <= k &&  k <= nums[hi] ) lo = mid+1 ;
+                else hi = mid-1;
+            }
+         }
 
+         return -1;
+     
     }
 };
