3fd03f551c4890b7373bcbbbbcf5d541b4f6f97c
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-06-13 08:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (90.33%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -4,6 +4,7 @@ public:
         int n =  arr.size() ; 
         for(int i  = 0  ; i  <   n  ; i++) {
             if(arr[i] <= k )  k++ ;
+            else break ; 
         }
 
         return  k ;
