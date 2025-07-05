0355077d950eba116002010f8e8a237e7f3ae582
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-06 00:35:27 +0530

    Time: 3 ms (41.13%) | Memory: 15.4 MB (17.55%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
- void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-  if (target == 0) {
-    ans.push_back(ds);
-    return;
-  }
-  for (int i = ind; i < arr.size(); i++) {
-    if (i > ind && arr[i] == arr[i - 1]) continue;
-    if (arr[i] > target) break;
-    ds.push_back(arr[i]);
-    findCombination(i + 1, target - arr[i], arr, ans, ds);
-    ds.pop_back();
-  }
-}
-vector < vector < int >> combinationSum2(vector < int > & candidates, int target) {
-  sort(candidates.begin(), candidates.end());
-  vector < vector < int >> ans;
-  vector < int > ds;
-  findCombination(0, target, candidates, ans, ds);
-  return ans;
+    void helper(vector<int>& arr , vector<vector<int>>& ans , vector<int> ds , int idx , int k)  {
+       if(k == 0 ){
+        ans.push_back(ds) ;
+        return; 
+       }
 
+       for(int i  = idx ;i<arr.size();i++ ){
+        if(i > idx && arr[i] == arr[i-1]) continue ; 
+        if(arr[i] >  k )  break ;
+
+        ds.push_back(arr[i]) ; 
+        helper(arr,ans,ds,i+1,k - arr[i]); 
+        ds.pop_back() ;
+
+       }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& arr, int k) {
+        sort(arr.begin() , arr.end()) ;  
+        vector<vector<int>> ans ; 
+        vector<int>ds; 
+        helper(arr,ans,ds,0,k) ; 
+        return ans  ; 
     }
 };
