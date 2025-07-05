3e587ed513f5b1ca367da7267e3cd6b2d0085d34
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-06 00:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (33.44%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    void findSubset(vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds, int idx) {
-        ans.push_back(ds);
-        
-        for (int i = idx; i < nums.size(); i++) {
-            if (i > idx && nums[i] == nums[i - 1]) continue;
-            ds.push_back(nums[i]);
-            findSubset(nums, ans, ds, i + 1);
-            ds.pop_back();
+    void helper(vector<int>& nums , vector<vector<int>>& ans , vector<int>ds  , int idx){
+        ans.push_back(ds); 
+        for(int i =  idx  ; i < nums.size() ; i++){
+          if(i >idx && nums[i] == nums[i-1]) continue;
+          ds.push_back(nums[i]) ; 
+          helper(nums,ans,ds,i+1) ;
+          ds.pop_back()  ;   
         }
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findSubset(nums, ans, ds, 0);
+        sort(nums.begin() , nums.end()) ; 
+        vector<vector<int>>ans ; 
+        vector<int>ds ;
+        helper(nums,ans,ds,0);
         return ans;
     }
 };
