0bd8d131f11da78fc22b4fad12f02500ec94d6a8
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-08 17:46:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.97%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,18 +1,9 @@
 class Solution {
-private:
-    int countSetBits(long long n) {
-        int count = 0;
-        while (n) {
-            if (n & 1) count++;
-            n = n >> 1;
-        }
-        return count;
-    }
+
+   
 
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) return false; // Add this to handle negative or 0 input
-        int noOfSetBits = countSetBits(n);
-        return noOfSetBits == 1;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
