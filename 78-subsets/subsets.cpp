310adc6ac55a5326190d1429abcdbbc4327a49bb
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-09 18:29:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (60.89%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void helper(vector<int>& nums , vector<vector<int>>& ans , vector<int>v  , int idx){
-      if(idx ==  nums.size()) {
-        ans.push_back(v);
-        return  ;
-      }
-      //we follow take and not take approch 
-      helper(nums,  ans , v,idx+1) ; 
-      v.push_back(nums[idx]) ; 
-      helper(nums,  ans , v,idx+1) ; 
-
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n  =  nums.size() ; 
-        vector<vector<int>> ans ; 
-        vector<int>v ; 
-        helper(nums, ans, v , 0) ; 
-
-
-        return ans ; 
+        vector<vector<int>> result;
+        int n = nums.size();
+        
+        // Loop through all possible subsets (2^n possibilities)
+        for (int i = 0; i < (1 << n); i++) {
+            vector<int> subset;
+            
+            // For each bit in i, decide whether to include nums[j]
+            for (int j = 0; j < n; j++) {
+                // Check if the j-th bit of i is set (1) or not (0)
+                if (i & (1 << j)) {
+                    subset.push_back(nums[j]);
+                }
+            }
+            
+            // Add the current subset to the result
+            result.push_back(subset);
+        }
+        
+        return result;
     }
 };
