343ac7ecb957e4097e7bfdeb47bfc9f7e05b6a83
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-19 13:06:45 +0530

    Time: 3 ms (43.24%) | Memory: 12.9 MB (37.97%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-  vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-    unordered_map<int, int> mp;
-    stack<int> st;
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;
+        stack<int> st;
+        int n = nums2.size();
 
-    for(int i=nums2.size()-1; i>=0; i--){
-      while(!st.empty() && st.top() < nums2[i]){
-        st.pop();
-      }
-      if(st.empty() || st.top() < nums2[i]) mp[nums2[i]] = -1;
-      else mp[nums2[i]] = st.top();
-      st.push(nums2[i]);
+        // Handle last element manually
+        st.push(nums2[n - 1]);
+        mp[nums2[n - 1]] = -1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                mp[nums2[i]] = -1;
+            } else {
+                mp[nums2[i]] = st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for (int num : nums1) {
+            ans.push_back(mp[num]);
+        }
+
+        return ans;
     }
-
-    vector<int> solution;
-    for(int i=0; i<nums1.size(); i++){
-      solution.push_back(mp[nums1[i]]);
-    }
-
-
-    return solution;
-  }
 };
