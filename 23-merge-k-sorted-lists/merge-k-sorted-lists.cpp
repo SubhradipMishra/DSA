8a55d6bb64604ce3f3ae7e683e491a393ef7529b
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-07-31 16:39:56 +0530

    Time: 4 ms (47.90%) | Memory: 18.4 MB (82.16%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; // min-heap
+        }
+    };
+
+    priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+    for (auto list : lists) {
+        if (list) pq.push(list);
+    }
+
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+
+    while (!pq.empty()) {
+        ListNode* smallest = pq.top(); pq.pop();
+        tail->next = smallest;
+        tail = tail->next;
+
+        if (smallest->next) {
+            pq.push(smallest->next);
+        }
+    }
+
+    return dummy.next;
+}
+
+} ;
