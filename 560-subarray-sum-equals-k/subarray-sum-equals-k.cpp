86a93e87d56c76e9fddbed5b858d2f3dff98dd84
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-08-02 21:14:01 +0530

    Time: 35 ms (90.04%) | Memory: 46.9 MB (38.14%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixSumCount;
-    prefixSumCount[0] = 1; // base case: subarray starting from index 0
-    int prefixSum = 0, count = 0;
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size(); 
+        int count = 0; 
+        unordered_map<int, int> mp;
+        vector<int> pre(n); 
 
-    for (int num : nums) {
-        prefixSum += num;
-        if (prefixSumCount.find(prefixSum - k) != prefixSumCount.end()) {
-            count += prefixSumCount[prefixSum - k];
+        pre[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + arr[i];
         }
-        prefixSumCount[prefixSum]++;
-    }
 
-    return count; 
+        mp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            int currSum = pre[i];
+            int rem = currSum - k;
+            if (mp.find(rem) != mp.end()) {
+                count += mp[rem];
+            }
+            mp[currSum]++;
+        }
+
+        return count;
     }
 };
