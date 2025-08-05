63fe008f2e62dae6d032943e49bda1957904c619
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-08-05 18:31:50 +0530

    Time: 1561 ms (19.92%) | Memory: 72 MB (84.79%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void helper(vector<int>& colSum , int first , int last , int seat){
+        for(int i = first ; i <= last ; i++){
+            colSum[i] += seat ; 
+        }
+    }
+
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        int row = bookings.size(); 
+        int col = n;
+
+        vector<int> colSum(n, 0); 
+
+        for(int i = 0 ; i < row ; i++ ){
+            int first = bookings[i][0] - 1; // Convert to 0-based index
+            int last  = bookings[i][1] - 1; // Convert to 0-based index
+            int seat  = bookings[i][2];
+
+            helper(colSum, first, last, seat); 
+        }
+
+        return colSum; 
+    }
+};
