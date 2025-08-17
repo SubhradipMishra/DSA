91d876c63b686a954276dd19d02118274daeadf3
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-08-17 19:10:20 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (48.04%) - LeetSync

---
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int helper(TreeNode* root, int& maxi) {
+        if (!root) return 0;
+
+        int leftSum = max(0, helper(root->left, maxi));
+        int rightSum = max(0, helper(root->right, maxi));
+
+    
+        maxi = max(maxi, root->val + leftSum + rightSum);
+
+     
+        return root->val + max(leftSum, rightSum);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        helper(root, maxi);
+        return maxi;
+    }
+};
