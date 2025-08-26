23febfee28bf7150d7688eb8c96ce3f14ecd3ce5
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-08-26 10:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (94.05%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -3,20 +3,20 @@ public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
         int n = dimensions.size(); 
         int ans = INT_MIN; 
-        double sq = -1;   // to store max diagonal length (should be double)
+        double sq = -1;  
 
         for(int i = 0; i < n; i++){
             int a = dimensions[i][0];
             int b = dimensions[i][1];
 
-            // correct diagonal formula
+           
             double x = sqrt(a * a + b * b);
 
-            if(x > sq){  // found strictly larger diagonal
+            if(x > sq){  
                 sq = x;
                 ans = a * b;
             }
-            else if(x == sq){ // same diagonal → choose larger area
+            else if(x == sq){ 
                 ans = max(ans, a * b);
             }
         }
