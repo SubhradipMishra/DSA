bc111716b9445fda1d369ba744c40f1d41ba2bdc
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-08-26 16:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (64.52%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,24 +1,25 @@
-#include <vector>
-
 class Solution {
 public:
-    std::vector<std::vector<int>> generate(int numRows) {
-        std::vector<std::vector<int>> result;
-        
-        for (int i = 0; i < numRows; i++) {
-            std::vector<int> row(i + 1); // Create a vector of size i+1 for the current row
-            
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    row[j] = 1; // First and last element of each row is 1
-                } else {
-                    row[j] = result[i-1][j-1] + result[i-1][j]; // Calculate value based on the previous row
-                }
-            }
-            
-            result.push_back(row); // Push the row vector into the result vector
+     vector<int>generateRow(int row){
+        long long ans =  1 ; 
+        vector<int> res ; 
+        res.push_back(ans) ; 
+        for(int col  =  1 ; col < row ; col++)
+        {
+            ans = ans *(row-col) ;
+            ans = ans/(col) ; 
+            res.push_back(ans); 
         }
+        return res; 
+     }
+    vector<vector<int>> generate(int n) {
+          vector<vector<int>>ans ; 
+          for(int i  = 1 ; i <=  n ; i++){
+           ans.push_back(generateRow(i)) ;
+
         
-        return result;
+          }
+
+          return ans ; 
     }
 };
