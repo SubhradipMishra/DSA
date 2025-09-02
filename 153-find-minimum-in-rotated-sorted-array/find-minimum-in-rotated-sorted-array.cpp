5a489f87215d3a43cd6fe010cf06652a32f7fe0e
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-03 02:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (79.40%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int lo = 0, hi = n - 1;
+        int n =  nums.size() ; 
+        int lo =  0 ;
+        int hi = n-1;
+        while(lo <= hi ){
+            if(nums[lo] <= nums[hi] ) return nums[lo ];
+            int mid = lo + (hi-lo)/2;
+            int next = (mid+1)%n;
+            int prev = (mid-1+n)%n;
+            if(nums[mid] <= nums[next ]&& nums[mid] <=  nums[prev]) return nums[mid]  ; 
+            else if(nums[lo] <= nums[mid]) lo  = mid +1;
+            else hi  = mid-1;
 
-        while (lo <= hi) {
-            // If array is already sorted
-            if (nums[lo] <= nums[hi])
-                return nums[lo];
-
-            int mid = lo + (hi - lo) / 2;
-            int next = (mid + 1) % n;
-            int prev = (mid - 1 + n) % n;
-
-            // Pivot element check
-            if (nums[mid] <= nums[next] && nums[mid] <= nums[prev])
-                return nums[mid];
-
-            // Decide the unsorted part
-            if (nums[lo] <= nums[mid])
-                lo = mid + 1;
-            else
-                hi = mid - 1;
         }
 
-        return -1; // Should never hit this for valid input
+        return -1;
+
     }
 };
