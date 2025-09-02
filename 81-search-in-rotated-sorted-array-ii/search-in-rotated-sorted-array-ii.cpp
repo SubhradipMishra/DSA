92b8d9a14255211d43c2c74b908af5dc574c3f19
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-03 02:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (20.28%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int k) {
-        int  n = nums.size() ; 
-        int  lo = 0 ; 
-        int  hi = n-1; 
-        while(lo <= hi){
-            int mid =  lo + (hi - lo )/2 ; 
-            if(nums[mid] == k  ) return true ; 
-            else if(nums[lo] == nums[mid] && nums[hi] ==  nums[mid] ){
-                lo++ ;
-                hi-- ;
+    bool search(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target) return true;
+
+            // If duplicates, shrink
+            if (nums[lo] == nums[mid] && nums[mid] == nums[hi]) {
+                lo++;
+                hi--;
             }
-            else if(nums[lo] <= nums[mid]){
-                if(nums[lo] <= k && k < nums[mid]) hi = mid-1;
-                else lo = mid+1;
+            // Left half sorted
+            else if (nums[lo] <= nums[mid]) {
+                if (nums[lo] <= target && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
             }
-            else{
-                if(nums[mid] < k && k <= nums[hi]) lo = mid + 1;
-                else hi = mid-1;
+            // Right half sorted
+            else {
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
             }
         }
-
         return false;
     }
 };
