c134f57753764ebccbb2901f09f28da94318cc45
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-03 01:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (17.71%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
-    int findFirstOccurence(vector<int>& a , int k ) {
-        int lo =  0 ; 
-        int hi =  a.size()-1 ;
-        while(lo <= hi ) {
-            int mid = lo + (hi-lo)/2; 
-            if(a[mid] == k ){
-              if(  mid == 0 || a[mid - 1] < a[mid]   ) return mid;
-              else hi = mid-1  ; 
-
+     int firstOccurence (vector<int>arr , int k){
+        int lo = 0 ;
+        int hi = arr.size()-1 ; 
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2 ;
+            if(arr[mid] == k ){
+                if(mid == 0 || arr[mid] != arr[mid-1] ) return mid; 
+                else hi = mid-1;
             }
-            else if(a[mid]  <   k ) lo = mid +1;
-            else hi = mid- 1; 
+            else if(arr[mid] < k ) lo = mid+1;
+            else hi = mid-1; 
         }
 
         return -1;
-    }
+     }
 
-
-
-    int findLastOccurence(vector<int>& a , int k ) {
-        int lo =  0 ; 
-        int hi =  a.size()-1 ;
-        while(lo <= hi ) {
-            int mid = lo + (hi-lo)/2; 
-            if(a[mid] == k ){
-              if (    mid ==  a.size()-1 || a[mid + 1]  > a[mid]   ) return mid;
-              else lo = mid+ 1  ; 
-
+      int lastOccurence (vector<int>arr , int k){
+        int lo = 0 ;
+        int hi = arr.size()-1 ; 
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2 ;
+            if(arr[mid] == k ){
+                if(mid == arr.size()-1 || arr[mid] != arr[mid+1] ) return mid; 
+                else lo = mid+1;
             }
-           else if(a[mid]  <   k ) lo = mid +1;
-            else hi = mid- 1; 
+            else if(arr[mid] < k ) lo = mid+1;
+            else hi = mid-1; 
         }
 
         return -1;
-    }
-    vector<int> searchRange(vector<int>& nums, int k) {
-       
-       int firstOccurence = findFirstOccurence(nums,k) ; 
-       int lastOccurence  = findLastOccurence(nums,k) ; 
+     }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        vector<int>ans(2,-1); 
+        int f = firstOccurence(nums,target);
+        if(f == -1) return ans ; 
+        int l = lastOccurence(nums,target);
+        ans[0] = f;
+        ans[1] = l;
 
-       vector<int>ans; 
-       ans.push_back(firstOccurence);
-       ans.push_back(lastOccurence);
-
-       return ans;  
-       
+        return ans ; 
     }
 };
