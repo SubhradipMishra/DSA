a1fc60baed0b72a7af07d1d17692593bdbf9534b
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-04 21:29:55 +0530

    Time: 4 ms (88.42%) | Memory: 23 MB (41.76%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    bool check(vector<int>& piles,int h , int x ){
-        int count = 0 ;
-        int n =  piles.size() ; 
-         for(int i  = 0  ;  i <  n; i++ ) {
-              if(count > h ) return false; 
-            if(piles[i] % x ==  0 ) count+= piles[i] /x; 
-            else count+=(piles[i] /x) +1 ;
-
-
-         }
-
-         return count <= h;
-        
+   bool check(vector<int>& piles,int mid , int h){
+    int n = piles.size(); 
+    int count = 0 ; 
+    for(int i =  0 ; i < n ; i++ ){
+         if(count > h )  return false ;
+         if(piles[i] <= mid) count++; 
+         else{
+            if(piles[i] % mid == 0 ) count+= piles[i]/mid ;
+            else count+= (piles[i]/mid) + 1; 
     }
+   
+    } 
+     return count <= h ; 
+   }
     int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int n = piles.size() ; 
-        int lo = 1 ; 
-        int maxi = -1;
-        for(int  i =  0; i <  n  ; i++  ){
-            if(maxi  <  piles[i] ) maxi = piles[i] ; 
-
+        int n =  piles.size() ;
+        int maxi = INT_MIN ;
+        for(int i  = 0 ; i < n  ;i++){
+            maxi = max(maxi , piles[i]) ;
         }
+
+        int lo = 1 ;
         int hi = maxi ; 
-        int ans = -1;
+
+        int ans = -1 ; 
+
         while(lo <= hi ){
-            int mid = lo + (hi-lo)/2;
-            if(check(piles,h,mid)){
+            int mid = lo +(hi-lo)/2 ;
+            if(check(piles,mid,h))
+            {
                 ans = mid ; 
-                hi = mid-1;
+                hi = mid - 1  ;
             }
-            else lo = mid+1; 
+            else lo = mid + 1 ;
         }
 
-        return ans ; 
+        return ans ;
     }
 };
