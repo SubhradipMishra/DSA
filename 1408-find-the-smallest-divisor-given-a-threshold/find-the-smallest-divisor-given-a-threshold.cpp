c73406159aacbf535a271c0f96713c9ce2e3d13e
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-07 01:25:25 +0530

    Time: 11 ms (46.20%) | Memory: 26.2 MB (46.52%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    bool check(vector<int>& nums , int t , int x) {
-        int sum =  0 ; 
-        for(int i  = 0 ; i <  nums.size() ; i++ ) {
-            if(sum >  t )  return false ; 
-            if(nums[i] % x == 0 )  sum +=  nums[i]/x; 
-            else sum+= nums[i]/x +1 ;
+    bool check(vector<int>& nums , int mid , int t){
+        int count = 0; 
+        for(int i  = 0;  i < nums.size() ; i++){
+            if(count >  t )  return false ;
+            if(nums[i] <=  mid )  count+=1 ; 
+            else{
+                if(nums[i] % mid == 0 ) count += nums[i]/mid ; 
+                else count+= ((nums[i]/mid) + 1) ; 
+            }
+
+            
         }
 
-        return t >= sum ; 
+        return count <= t ; 
     }
     int smallestDivisor(vector<int>& nums, int t) {
-        int n  =  nums.size() ; 
-       
-        int maxi = INT_MIN ;
-        for(int i  = 0 ; i < n   ; i++ ) {
-            maxi  =  max(nums[i] , maxi) ; 
-
-
+        int n = nums.size() ; 
+        int lo = 1; 
+        int maxi = INT_MIN ; 
+        for(int i =  0;  i <  n ; i++){
+            maxi= max(nums[i] , maxi) ; 
         }
 
-        int lo = 1 ; 
-        int hi =  maxi;  
-        int ans = -1;
+        int hi = maxi; 
+        int ans = -1 ; 
 
-        while(lo <= hi ) {
-            int mid = lo + (hi - lo)/2 ;
-            if(check(nums ,t, mid ))  {
-                ans = mid ; 
-                hi = mid - 1;
+
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2 ;
+            if(check(nums,mid,t)){
+                ans = mid; 
+                hi= mid-1 ;
             }
-            else lo = mid+1;
+            else {
+                lo = mid+1;
+            }
         }
 
-        return ans; 
+
+
+        return ans ; 
+
     }
 };
