a1e118b474fcd39a861a1c3cb1b0067a07f40221
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-10 15:53:12 +0530

    Time: 3 ms (44.52%) | Memory: 9.3 MB (81.09%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
+        if(s.size() != t.size()) return false;
 
-        unordered_map<char, char> mapST, mapTS;
+        unordered_map<char,char> mp1; // s -> t
+        unordered_map<char,char> mp2; // t -> s
 
-        for (int i = 0; i < s.size(); i++) {
+        for(int i = 0; i < s.size(); i++) {
             char c1 = s[i], c2 = t[i];
 
-            if (mapST.count(c1) && mapST[c1] != c2) return false;
-            if (mapTS.count(c2) && mapTS[c2] != c1) return false;
+            if(mp1.find(c1) != mp1.end() && mp1[c1] != c2) return false;
+            if(mp2.find(c2) != mp2.end() && mp2[c2] != c1) return false;
 
-            mapST[c1] = c2;
-            mapTS[c2] = c1;
+            mp1[c1] = c2;
+            mp2[c2] = c1;
         }
-        
+
         return true;
     }
 };
