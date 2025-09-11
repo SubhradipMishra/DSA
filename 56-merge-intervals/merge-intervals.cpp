047014314644da08d35c2aaf214aa896064e923a
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-11 18:02:54 +0530

    Time: 5 ms (55.07%) | Memory: 23.9 MB (57.49%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin() , intervals.end()) ; 
-        int n = intervals.size() ; 
+        // sort(intervals.begin() , intervals.end()) ; 
+        // int n = intervals.size() ; 
+        // vector<vector<int>>ans ; 
+        // for(int i  =  0  ; i <  n ; i++ ){
+        //      int x = ans.size()-1; 
+        //      if( x <  0 ||  ans[x][1]  <  intervals[i][0])
+        //      {
+        //         ans.push_back(intervals[i]) ;
+        //      }
+        //      else{
+        //         ans[x][1] = max(ans[x][1]  ,  intervals[i][1]) ;
+        //      }
+        // }
+
+        // return ans ;
+
+
+        sort(intervals.begin() , intervals.end());
+        int n = intervals.size() ;
         vector<vector<int>>ans ; 
-        for(int i  =  0  ; i <  n ; i++ ){
-             int x = ans.size()-1; 
-             if( x <  0 ||  ans[x][1]  <  intervals[i][0])
-             {
+        for(int i = 0 ; i < n ;i++){
+            int x = ans.size()-1 ;
+            if(x < 0 || ans[x][1] < intervals[i][0]){
                 ans.push_back(intervals[i]) ;
-             }
-             else{
-                ans[x][1] = max(ans[x][1]  ,  intervals[i][1]) ;
-             }
+            }
+            else ans[x][1] = max(ans[x][1] , intervals[i][1]) ;
         }
 
-        return ans ;
+        return ans ; 
     }
 };
