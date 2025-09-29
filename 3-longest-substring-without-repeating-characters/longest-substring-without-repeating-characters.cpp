eb8d7a8945ea367eb93704a6c8f073c45cddecfe
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-09-29 17:29:19 +0530

    Time: 3 ms (81.96%) | Memory: 11.7 MB (66.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int i = 0;
-        int j = 0 ; 
-        int maxLen = 0 ;
-        map<char,int>mp ;
-        while(j < n ){
-            if(mp.find(s[j]) ==  mp.end()){
-                mp[s[j]] = j;
+        unordered_map<char,int>mp  ; 
+        int n = s.size(); 
+        int i = 0 ;
+        int j = 0 ;
+        int maxLen = 0 ; 
+        while( j < n ){
+            if(mp.find(s[j]) == mp.end()){
+                mp[s[j]] = j ; 
             }
             else{
-                i = max(i , mp[s[j]]+1) ;
-                mp[s[j]] = j ;
+              i  = max(i  , mp[s[j]]+1) ;
+              mp[s[j]] = j ; 
             }
 
-            maxLen = max(maxLen , j-i+1);
+            maxLen = max(maxLen , j-i+1 ) ;
             j++ ;
         }
 
