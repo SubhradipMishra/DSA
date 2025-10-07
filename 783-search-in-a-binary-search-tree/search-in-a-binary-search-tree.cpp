4f98050caf4e7f5bd0de4240a3d106e4f5415705
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-10-07 10:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (61.31%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -1,14 +1,12 @@
 
 class Solution {
-public: TreeNode* helper(TreeNode* root  , int val){
-    if(!root) return nullptr ; 
-    if(root->val == val) return root ; 
-    
-    if(root->val > val )return helper(root->left,val) ; 
-    else return helper(root->right  ,val) ;
-}
+public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(!root) return nullptr ; 
-        return helper(root  , val) ;
-     }
+        if(!root) return nullptr;
+        if(val == root->val) return root; 
+        if(root->val < val) return searchBST(root->right ,val) ;
+        return searchBST(root->left , val);
+
+
+    }
 };
