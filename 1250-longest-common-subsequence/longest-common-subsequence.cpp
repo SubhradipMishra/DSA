810a9cdf892a9e24eaa95eac7bab0849478824ca
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-11-14 10:59:27 +0530

    Time: 43 ms (23.37%) | Memory: 27.6 MB (28.84%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    // int f (int   i , int  j , string& s , string& t , vector<vector<int>>& dp){
-    //     if( i <  0 ||  j < 0 )  return 0 ;
-    //     if(dp[i][j]!=-1) return dp[i][j] ; 
-    //     if(s[i] == t[j]) return  dp[i][j]  =  1 + f(i-1 ,j -1, s, t ,dp) ;
-    //     return dp[i][j]  =  0  + max(f(i-1 ,  j , s, t,dp ) , f(i , j - 1 , s , t ,dp)) ; 
-    // }
+    int f(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == t[j])
+            return dp[i][j] = 1 + f(i - 1, j - 1, s, t, dp);
+        else
+            return dp[i][j] = max(f(i - 1, j, s, t, dp), f(i, j - 1, s, t, dp));
+    }
+
     int longestCommonSubsequence(string s, string t) {
-        int n =  s.size() ;
-        int m =  t.size() ; 
-       vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-
-        vector<int>prev(m+1 , 0 ) ;
-        vector<int>cur(m+1 , 0) ; 
-        // base case
-             
-      
-        for(int j  =  0;  j <= m ; j++ ) prev[j] = 0  ;
-        for(int   i = 1  ;i <= n ;  i++ ) {
-            for(int j =  1; j  <=   m ; j++ ) {
-                if(s[i-1] == t[j-1] ) cur[j]= 1 + prev[j-1] ;
-                else cur[j] = max(prev[j] , cur[j-1]) ; 
-            }
-            prev = cur ;   ;  
-        }
-
-
-        return prev[m] ; 
-
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(n - 1, m - 1, s, t, dp);
     }
 };
