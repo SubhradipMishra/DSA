d89b6219aa53944b3caf9563c5f7c22c1ed26a7e
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-11-19 18:33:58 +0530

    Time: 3 ms (12.06%) | Memory: 13.4 MB (14.82%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    // int f(int i , int j ,vector<vector<int>>& t  , vector<vector<int>>& dp){
-    //     if(i == t.size()-1) return t[t.size()-1][j] ;
-    //     if(dp[i][j]!=-1) return dp[i][j] ; 
-    //     int d = t[i][j] + f(i+1,j,t,dp) ;
-    //     int dg = t[i][j] + f(i+1,j+1,t,dp) ;
-    //     return dp[i][j] = min(d,dg) ; 
-    // }
-int minimumTotal(vector<vector<int>>& triangle) {
-    int n = triangle.size();
-    
-    
-    vector<int> front(n), cur(n);
 
-    
-    for(int j = 0; j < n; j++) 
-        front[j] = triangle[n-1][j];
+    int helper(int i , int   j , int n  , vector<vector<int>>& triangle , vector<vector<int>>& dp){
 
-    for(int i = n-2; i >= 0; i--) {
-        for(int j = i; j >= 0; j--) {
-            int d = triangle[i][j] + front[j];
-            int dg = triangle[i][j] + front[j+1];
-            cur[j] = min(d, dg);
-        }
-        front = cur;
+        if(i == n-1 ) return triangle[i][j] ; 
+        if(dp[i][j]!=-1) return dp[i][j] ; 
+        int digo = triangle[i][j] + helper(i + 1  ,  j+1 , n , triangle,dp) ; 
+        int down = triangle[i][j] + helper(i + 1  ,  j , n , triangle , dp) ; 
+
+        return dp[i][j] = min(digo , down) ;
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size() ;
+        vector<vector<int>>dp(n , vector<int>(n,0)) ;
+
+    // base case
+    
+    for(int j   = 0  ; j  < n ; j++){
+        dp[n-1][j] = triangle[n-1][j] ;
     }
 
-    return front[0];
-}
+    for(int i   =  n-2 ; i>= 0 ; i--){
+        for(int j =  i ; j >= 0 ; j--){
+            int down = dp[i+1][j]  + triangle[i][j]  ;  
+            int digo = dp[i+1][j+1] + triangle[i][j] ;
+
+            dp[i][j] = min(down , digo) ;  
+        }
+    }
+
+    return dp[0][0] ;
+    }
 };
