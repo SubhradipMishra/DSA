e2ee60ddf0738f8611e86f202983693a9c7ad0c4
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-11-23 17:31:29 +0530

    Time: 199 ms (12.77%) | Memory: 204 MB (9.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-   bool f(int i, int k, vector<int>& arr, vector<vector<int>>& dp) {
-    if (k == 0) return true;
-    if (i == 0) return (arr[0] == k);
-    if (dp[i][k] != -1) return dp[i][k];
 
-    bool nonTake = f(i-1, k, arr, dp);
-    bool take = false;
-    if (arr[i] <= k) take = f(i-1, k-arr[i], arr, dp);
+    bool helper(vector<int>& arr , int sum ,int idx , vector<vector<int>>& dp) {
+        if(sum == 0)  return true;  
+        if(idx == arr.size()) return false ; 
+        if(dp[idx][sum]!=-1) return dp[idx][sum] ; 
+        bool take = false ; 
+        if(arr[idx] <= sum )  take = helper(arr , sum-arr[idx] , idx+1,dp);
+        bool nottake = helper(arr , sum , idx+1,dp);
 
-    return dp[i][k] = take || nonTake;
-}
-    bool canPartition(vector<int>& nums) {
-        int n  = nums.size();
-        int sum  = 0 ; 
-        for(int i =  0 ; i <  n ; i++){
-            sum+=nums[i] ; 
-        } 
-        if(sum %2 != 0  )  return false  ;
-        int k  = sum/2 ;
-        vector<vector<int>> dp(n, vector<int>(k+1, false));
-        // base case
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-         }
-         for (int i = 1; i < n; i++) {
-        for (int target = 1; target <= k; target++) {
-            bool nonTake = dp[i-1][target];
-            bool take = false;
-            if (nums[i] <= target) 
-                take = dp[i-1][target-nums[i]];
-
-            dp[i][target] = take || nonTake;
-        }
+        return dp[idx][sum] =  take | nottake ; 
     }
+    bool canPartition(vector<int>& arr) {
 
-    return dp[n-1][k];
+
+     int n = arr.size() ; 
+
+     int sum = 0 ; 
+     for(int i  = 0 ; i < n;  i++){
+        sum += arr[i] ; 
+
+     }  
+
+
+
+     if(sum%2!=0 )  return false ; 
+
+     vector<vector<int>>dp(n , vector<int>(sum+1, -1)) ; 
+     return helper(arr , sum/2 ,0,dp); 
     }
 };
