6889a54498e7fe69448c1d9fe3ec76ee49ecec23
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2025-12-10 21:37:36 +0530

    Time: 51 ms (66.85%) | Memory: 24.6 MB (57.87%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& mat) {
-         priority_queue<pair<int,pair<int,int>>,
-        vector<pair<int,pair<int,int>> > ,
-        greater<pair<int,pair<int,int >>>> pq;
-        
-        int n = mat.size() ; 
-        int m = mat[0].size() ;
-        
-        vector<vector<int>> dist(n , vector<int>(m,1e9));
-        dist[0][0] =  0;
-        
-        pq.push({0, {0,0}}) ; 
-        
-        
-        int dr[] = {-1,0,1,0} ;
+        priority_queue<pair<int,pair<int,int>>,
+        vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>> pq ; 
+
+        int n =  mat.size() ;
+        int m =  mat[0].size() ; 
+
+        vector<vector<int>>dist(n , vector<int>(m ,1e9)) ;
+
+        pq.push({0 , {0,0}}) ;
+        dist[0][0] = 0 ; 
+
+
+        int dr[] = {-1,0,1,0} ; 
         int dc[] = {0,1,0,-1} ; 
-        
-        
+
         while(!pq.empty()){
-            auto it = pq.top() ;
-            pq.pop() ; 
-            
-            int diff = it.first ;
-            int row  = it.second.first ; 
-            int col  = it.second.second ; 
-            
-            if(row == n-1 && col == m-1)  return diff ; 
-            
-            
-            for(int i =   0 ; i <  4 ;i++){
-                int newr = row + dr[i] ;
-                int newc = col + dc[i] ; 
-                
+          auto it = pq.top() ; 
+          pq.pop() ;
+
+          int dis = it.first ; 
+          int row = it.second.first  ;
+          int col = it.second.second ; 
+
+          if(row == n-1 && col == m-1)  return dis ; 
+
+          for(int i  =  0 ; i < 4 ; i++){
+               int newr = row +  dr[i] ; 
+               int newc = col +  dc[i] ;
+
                 if(newr >= 0 && newc >= 0 && newr < n && newc < m ){
-                   int newEffort = max(abs(mat[row][col] - mat[newr][newc]), diff);
- 
-                    if(newEffort < dist[newr][newc]){
-                        
-                    dist[newr][newc] = newEffort ; 
+                   int newEffort = max(abs(mat[row][col] - mat[newr][newc]), dis);
+                   if(newEffort < dist[newr][newc]){
+                    dist[newr][newc] =  newEffort  ;
                     pq.push({newEffort , {newr , newc}}) ; 
-                    }
-                }
-            }
-            
-            
+                   }
+
+          }
+
+          }
         }
-        
-        
-return dist[n-1][m-1];
+
+        return dist[n-1][m-1];
+
+
     }
 };
