8c47e5b834963be1f9923511c03512409287f726
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-01-15 20:55:50 +0530

    Time: 49 ms (25.98%) | Memory: 49.3 MB (27.37%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -14,28 +14,8 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size() ; 
-        vector<vector<int>>dp(n,vector<int>(amount+1 ,0)) ;
-        vector<int>prev(amount+1 ,0 );
-        vector<int>curr(amount+1,0) ; 
-
-        // base case
-
-        for(int T =  0 ; T  <= amount  ; T++ ){
-            if(T % coins[0] == 0 )  prev[T] = T/coins[0] ; 
-            else prev[T]  = 1e9 ; 
-        }
-
-        for(int i   =    1 ; i <  n ;  i++ ){
-            for(int t  =  0 ; t <= amount ; t++  ) {
-                int notTake =  0  + prev[t] ; 
-                int take = INT_MAX ; 
-                if(coins[i] <=  t )  take = 1 + curr[t - coins[i]] ; 
-                curr[t] = min(take,notTake) ; 
-            
-            }
-            prev =curr ; 
-        }
-        int ans  =  prev[amount] ;
+        vector<vector<int>>dp(n,vector<int>(amount+1 ,-1)) ;
+        int ans  =  f( n -1 ,amount ,coins,dp) ;
         if(ans >= 1e9)  return -1;
         return ans ; 
     }
