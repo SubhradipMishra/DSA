3a9d2748062ee096c6f6a8d6a355f79fcc7b1917
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-01-15 16:21:19 +0530

    Time: 115 ms (63.80%) | Memory: 110.6 MB (24.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
 
-    bool helper(vector<int>& arr , int sum ,int idx , vector<vector<int>>& dp) {
-        if(sum == 0)  return true;  
-        if(idx == arr.size()) return false ; 
-        if(dp[idx][sum]!=-1) return dp[idx][sum] ; 
-        bool take = false ; 
-        if(arr[idx] <= sum )  take = helper(arr , sum-arr[idx] , idx+1,dp);
-        bool nottake = helper(arr , sum , idx+1,dp);
+    bool helper(vector<int>& arr , int  k, int idx , int sum , vector<vector<bool>>& dp){
+        if(sum == k ) return true ; 
+        if(idx ==  arr.size()|| sum > k)  return false; 
 
-        return dp[idx][sum] =  take | nottake ; 
+        if(dp[idx][sum]) return dp[idx][sum] ; 
+
+        bool take = helper(arr , k , idx + 1 , sum +  arr[idx] , dp) ; 
+        bool notTake = helper(arr , k , idx + 1 ,sum , dp) ; 
+
+        return dp[idx][sum] = take | notTake ;
     }
     bool canPartition(vector<int>& arr) {
-
-
-     int n = arr.size() ; 
-
-     int sum = 0 ; 
-     for(int i  = 0 ; i < n;  i++){
-        sum += arr[i] ; 
-
-     }  
-
-
-
-     if(sum%2!=0 )  return false ; 
-
-     vector<vector<int>>dp(n , vector<int>(sum+1, 0 )) ; 
-     
-     // base case 
-
-     sum = sum/2 ; 
-     for(int i =  0 ; i< n ; i++) dp[i][0] = true;  
-     if(arr[0] <= sum) dp[0][arr[0]] = true ; 
-
-     for(int  i = 1  ; i < n  ;i++ ){
-        for(int s  = 1 ;s <= sum ; s++){
-            bool nottake = dp[i-1][s] ;
-            bool take = false ;
-             if(s >= arr[i]) take = dp[i-1][s-arr[i]] ; 
-
-             dp[i][s] = take | nottake ; 
+        int n =  arr.size() ; 
+        int sum  = 0 ; 
+        for(int i  =  0 ;  i < n ;i++){
+            sum += arr[i]  ;
         }
-     }
 
-     return dp[n-1][sum] ;
+        if(sum % 2!= 0 )  return false ;
+        int k = sum/2 ;
+
+        vector<vector<int>>dp(n ,vector<int>(k+1 , 0)) ; 
+
+        for(int i =  0 ; i  <  n ; i++) dp[i][0] =  true ; 
+        if(arr[0] <= k ) dp[0][arr[0]] = true ; 
+
+        for(int i =  1 ; i < n ; i++){
+            for(int s = 1 ; s <= k  ;s++){
+                bool nottake = dp[i-1][s];
+                bool take = false ; 
+                if(s >= arr[i])  take = dp[i-1][s-arr[i]] ; 
+
+                dp[i][s]  =take | nottake ; 
+            }
+        }
+
+        return dp[n-1][k] ; 
     }
 };
