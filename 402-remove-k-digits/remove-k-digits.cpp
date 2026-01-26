f38c2005440b5122578da27f4372b4e7968f0cca
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-01-26 19:05:42 +0530

    Time: 2 ms (70.11%) | Memory: 11.4 MB (66.54%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n  = num.size() ; 
-        stack<char>st ; 
-        for(int i  =  0 ;  i  < n  ; i++){
-            while(!st.empty() && k > 0 && (st.top()-'0') > (num[i]-'0')) {
-                st.pop(); 
-                k-- ; 
-            }
-            st.push(num[i]) ;
-        }
+       int n  = num.size() ; 
+       stack<char>st ; 
 
-        while(k != 0 && !st.empty()){
+       for(int i =    0 ; i <  n  ; i++){
+            while(!st.empty() && k > 0 && (st.top()-'0') > (num[i]-'0')) {
+                k-- ;
+                st.pop() ; 
+            }
+            st.push(num[i]) ; 
+       }   
+
+       while(k != 0 && !st.empty()){
             st.pop() ;
             k-- ;
         }
 
-
-        // creat the answer from stack by add element of stack ; 
-
-        string res = "";
+         string res = "";
         while(!st.empty()){
           res+=st.top(); 
           st.pop();  
         }
 
-        // if the number has leading zeros now the zeros has in the back of the res so remove them
-
-        while(res.size() != 0 && res.back() == '0'){
+         while(res.size() != 0 && res.back() == '0'){
             res.pop_back() ; 
         }
 
-        // reverse the res because we get it from stack
-
-        reverse(res.begin() , res.end()) ; 
+         reverse(res.begin() , res.end()) ; 
 
         if(res.size() == 0  ) return "0";
         else return res ; 
- 
     }
 };
