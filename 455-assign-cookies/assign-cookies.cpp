da20064e2b302580cfbbae7187151c2c062738fb
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-02-17 08:45:07 +0530

    Time: 15 ms (16.64%) | Memory: 44.8 MB (77.96%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-      sort(g.begin()  , g.end()) ; 
-      sort(s.begin() ,  s.end())  ;
-      int n = g.size() ;  
-      int m = s.size()  ; 
-      int count = 0 ; 
+      sort(g.begin() , g.end()) ; 
+      sort(s.begin()  , s.end()) ; 
 
-      int i =  0;// pointer for g
-      int j =  0 ; // pointer for s 
+      int n = g.size() ; 
+      int m = s.size(); 
+      int i =  0 ; // for greed array 
+      int j = 0 ; // for cookie size 
+      int count = 0 ;
 
-      while(  i < n && j < m ){
-        if(g[i] <= s[j] ){
+
+      while(i < n && j  < m ){
+        if( g[i] <= s[j] ) {
             count++;
-            i++;
+            i++ ;
         }
         j++;
       }
 
 
-      return count;
-      
+     return count ; 
 
     }
 };
