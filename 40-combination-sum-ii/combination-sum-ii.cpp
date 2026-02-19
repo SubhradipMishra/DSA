e08d8b5aacef6815362603746a65ee675338074e
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-02-19 19:38:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.87%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void helper(vector<int>& arr , vector<vector<int>>& ans , vector<int> ds , int idx , int k)  {
-       if(k == 0 ){
-        ans.push_back(ds) ;
-        return; 
-       }
 
-       for(int i  = idx ;i<arr.size();i++ ){
-        if(i > idx && arr[i] == arr[i-1]) continue ; 
-        if(arr[i] >  k )  break ;
+    void helper(vector<int>& arr , vector<vector<int>>& ans , vector<int>& v , int k , int idx){
+        if(k == 0 ){
+            ans.push_back(v) ;
+            return ; 
+        }
 
-        ds.push_back(arr[i]) ; 
-        helper(arr,ans,ds,i+1,k - arr[i]); 
-        ds.pop_back() ;
-
-       }
+        for(int i =  idx ; i <arr.size() ; i++ ){
+            if(arr[i] >  k ) break;
+            if(i > idx && arr[i] == arr[i-1] ) continue ; 
+            v.push_back(arr[i]);
+            helper(arr , ans , v , k - arr[i] , i+1);
+            v.pop_back() ; 
+        }
     }
     vector<vector<int>> combinationSum2(vector<int>& arr, int k) {
-        sort(arr.begin() , arr.end()) ;  
-        vector<vector<int>> ans ; 
-        vector<int>ds; 
-        helper(arr,ans,ds,0,k) ; 
-        return ans  ; 
+      sort(arr.begin() , arr.end()) ; 
+      int n = arr.size() ; 
+      vector<vector<int>>ans ; 
+      vector<int>v ; 
+      helper(arr,  ans, v , k , 0) ; 
+      return ans ;    
     }
 };
