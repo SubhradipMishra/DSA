34816cbc53d04ab2c3f8ca9e1beaafd14b6afe14
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-02-20 17:51:47 +0530

    Time: 315 ms (58.89%) | Memory: 8.7 MB (68.09%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-     bool check(int num , vector<vector<char>>& board , int i  ,int j ){
-         char ch = '0' + num ; 
-         for(int col  = 0 ; col < 9 ; col++){
-            if(board[i][col] == ch) return false ;
-         }
 
-         for(int row =  0 ; row < 9 ; row++){
-            if(board[row][j] == ch) return false ; 
+    bool isPossible(vector<vector<char>>& board ,int num ,  int i  , int j ){
+        char ch = '0' + num ; 
 
-         }
+        for(int row = 0 ; row < 9 ; row++){
+            if(board[row][j] ==  ch)  return false ;   
+        }
+        for(int col =  0 ; col < 9 ; col++){
+            if(board[i][col] == ch)  return false ; 
+        }
 
-         int sr = (i/3)*3 ; 
-         int sc = (j/3)*3 ; 
-
-         for(int  a = 0  ; a < 3 ; a++){
-            for(int b =  0 ; b < 3 ; b++){
-                if(board[sr + a][sc + b] == ch ) return false ; 
+        int sr = (i/3)*3 ; 
+        int sc = (j/3)*3 ;
+        for(int a = 0 ;a < 3 ;a++){
+            for(int b = 0 ; b  < 3; b++){
+                if(board[sr + a][sc+b] == ch) return false ; 
             }
-         }
+        }
 
-         return true ; 
-     }
-     bool solve(int i  , int  j  , vector<vector<char>>& board){
-        if( i == 9 )  return true ;
-        if( j == 9 )  return solve(i+1 , 0  ,board)  ; 
-        if(board[i][j] != '.')
-          return solve(i,j+1,board);
-        for(int num =1 ; num <=  9 ;num++){
-            if(check(num , board , i , j)){
-                board[i][j] = '0' +num ; 
-            if(solve(i,j+1 , board))
-                return true ; 
+        return true ; 
+    }
+    bool solve(int row , int col , vector<vector<char>>& board){
+        if(row == 9 ) return true ; 
+        if(col ==  9 )  return solve(row + 1 , 0 , board) ;
+        if(board[row][col] != '.') return solve(row , col + 1 , board) ; 
 
-             board[i][j] = '.' ; 
+        for(int i =  1 ; i <=  9 ; i++){
+            if(isPossible( board , i , row , col)){
+                 board[row][col] = '0' +  i ; 
+                 if(solve(row , col + 1 , board)) return true ;
+                 board[row][col]='.' ;
             }
         }
 
         return false ; 
-     }
+    }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(0,0,board) ;
+        solve(0,0,board) ; 
+
+
     }
 };
