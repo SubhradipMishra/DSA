e1ddc389aa1a146972d4eb111f5cb15bcdf8abbe
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-02-21 11:04:15 +0530

    Time: 21 ms (91.64%) | Memory: 16.1 MB (89.79%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        int m = grid.size();
-        int n = grid[0].size();
+    
 
-        // Base case: if out of bounds or current cell is water ('0')
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') return;
+    void dfs(vector<vector<char>>& grid , int i ,int j){
+        int n = grid.size() ; 
+        int m  = grid[0].size();
+        if(i < 0 || j < 0 || i >= n || j >= m  || grid[i][j] == '0' )  return  ;
 
-        // Mark the cell as visited
-        grid[i][j] = '0';
 
-        // Visit all 4 adjacent directions
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+         grid[i][j] = '0' ; 
+        dfs(grid , i  +1  , j ) ;
+        dfs(grid , i  , j +1 ) ; 
+        dfs(grid,i -1 , j ) ;
+        dfs(grid , i , j -1 ) ; 
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        if (m == 0) return 0;
-        int n = grid[0].size();
-
-        int count = 0;
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);  // sink the island
-                    count++;          // increment the number of islands
-                }
+        int n  = grid.size() ; 
+        int m =  grid[0].size() ; 
+    int count = 0 ;
+    if( n == 0 ) return 0  ;
+    for(int i  = 0 ; i < n ; i++){
+        for(int j  = 0 ; j < m ; j++){
+            if(grid[i][j] == '1' ) {
+                count++;
+                dfs(grid , i , j );
             }
         }
+    } 
 
-        return count;
+
+    return count ;
     }
 };
