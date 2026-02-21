54b8786513cfda4f05063da7f8cea3923b9df6a7
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-02-21 10:33:28 +0530

    Time: 3 ms (39.53%) | Memory: 20.9 MB (25.81%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adj, vector<bool>& visited, int st) {
-        visited[st] = true;
-        for (auto x : adj[st]) {
-            if (!visited[x]) {
-                dfs(adj, visited, x);
-            }
+
+      void dfs(vector<vector<int>>& adj , vector<bool>& visited , int st){
+        visited[st] = true ; 
+        for(auto x :  adj[st]) {
+            if(!visited[x])
+            dfs(adj  ,  visited , x) ; 
         }
-    }
-
+      }
     int findCircleNum(vector<vector<int>>& adj) {
-        int v = adj.size();
-
-        vector<vector<int>> adjList(v);
-
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++) {
-                if (adj[i][j] == 1 && i != j) {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+        int v =  adj.size() ; 
+        vector<vector<int>>adjList(v) ;
+        for(int i =  0  ; i  < v ;  i++  ){
+            for(int j =  0 ; j < v ; j++){
+                if(adj[i][j] == 1 && i!=j){
+                    adjList[i].push_back(j) ; 
+                    adjList[j].push_back(i) ; 
                 }
             }
-        }
+        } 
 
-        vector<bool> visited(v, false);
-        int count = 0;
+        vector<bool>visited(v,false) ;
+        int count = 0 ;
 
-        for (int i = 0; i < v; i++) {
-            if (!visited[i]) {
+        for(int i = 0 ; i <  v ; i++){
+            if(!visited[i]){
                 count++;
-                dfs(adjList, visited, i);
+                dfs(adjList , visited , i) ;
             }
         }
 
-        return count;
+        return count ; 
+
     }
 };
