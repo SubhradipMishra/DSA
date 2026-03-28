33b25eb26282aa62eab2e3948aae0077a68a38b1
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-03-28 11:41:28 +0530

    Time: 19 ms (79.96%) | Memory: 18 MB (70.37%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int f(int idx  ,int target , vector<int>& coins,vector<vector<int>>& dp){
-        if( idx == 0 ) {
-            if(target % coins[0] == 0 ) return target/coins[0] ; 
-            else return 1e9 ; 
-        }
-        if(dp[idx][target]!=-1) return dp[idx][target] ;
-        int notTake = 0 + f(idx - 1 ,target, coins,dp ) ;
-        int take = INT_MAX ; 
-        if(coins[idx] <= target) take = 1 +  f(idx, target-coins[idx] , coins,dp) ;
-        return dp[idx][target]=  min(take ,notTake) ;
-    }
     int coinChange(vector<int>& coins, int amount) {
+           vector<int> dp(amount + 1, amount + 1);
+
         
-        int n = coins.size() ; 
-        vector<vector<int>>dp(n,vector<int>(amount+1 ,-1)) ;
-        int ans  =  f( n -1 ,amount ,coins,dp) ;
-        if(ans >= 1e9)  return -1;
-        return ans ; 
+        dp[0] = 0;
+
+    
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+             
+                    dp[i] = min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+
+     
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
