67b3f59aae6891a0f43cdc5e1aeb175dd3473569
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-04-01 10:20:39 +0530

    Time: 1 ms (15.25%) | Memory: 9.9 MB (5.97%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
-    int maxRepeating(string sequence, string word) {
-        int n = sequence.size();
-        int m = word.size();
-        int maxi = 0;
-        for (int i = 0; i < n; i++) {
-            int k = i;
-            int count = 0;
-            while (k < n) {
-                string s = sequence.substr(k, m);
-                if (s == word) {
-                    count++;
-                    k += m;
-                } else {
-                    k++;
+    // Z-function
+    vector<int> buildZ(string s) {
+        int n = s.size();
+        vector<int> z(n, 0);
+        int l = 0, r = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                z[i]++;
+            }
+
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+
+    int maxRepeating(string text, string pattern) {
+
+        string temp = pattern;
+        int ans = 0;
+
+        while (true) {
+
+            string s = temp + "$" + text;
+            vector<int> z = buildZ(s);
+
+            bool found = false;
+
+            for (int i = 0; i < z.size(); i++) {
+                if (z[i] == temp.size()) {
+                    found = true;
                     break;
                 }
             }
-            maxi = max(maxi, count);
+
+            if (!found)
+                break;
+
+            ans++;
+            temp += pattern; 
         }
-        return maxi;
+
+        return ans;
     }
 };
