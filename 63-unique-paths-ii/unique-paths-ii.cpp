8fc3d3425c94c72a83f2210f563fad0d33534675
Subhradip Mishra <156043205+SubhradipMishra@users.noreply.github.com>
2026-05-08 10:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (52.84%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-  int helper(int i  ,int j , vector<vector<int>>& grid , vector<vector<int>>& dp){
-    if(i < 0 || j <0 || grid[i][j] == 1)  return 0 ; 
-    if( i == 0 &&  j == 0 ) return 1 ; 
-  
-    if(dp[i][j]!=-1) return dp[i][j] ; 
-    int down  = helper(i-1 , j , grid ,dp) ; 
-    int right = helper(i,j-1,grid, dp) ; 
-    return dp[i][j] = down + right ; 
-  }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int n =  grid.size() ;
-        int m =  grid[0].size() ;  
 
-        vector<vector<int>>dp(n+1 , vector<int>(m+1,-1))  ;
+    int helper(vector<vector<int>>& grid , int  i , int j , vector<vector<int>>& dp){
+        if( i < 0 ||  j < 0 ||   grid[i][j]  == 1) return 0 ; 
+        if( i == 0 &&  j ==0 )  return 1 ;
+        if(dp[i][j] != -1) return dp[i][j] ;
 
-        return helper(n-1,m-1,grid,dp) ;
-    
-    
-    
+        return dp[i][j] = helper(grid , i-1,j,dp) + helper(grid,  i , j -1 ,dp) ;  
+    }
+     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m  = grid.size() ; 
+        int n =  grid[0].size() ; 
+        vector<vector<int>> dp(m ,  vector<int>(n ,  -1))  ; 
+
+        return helper(grid , m -1 , n -1 , dp) ; 
     }
 };
